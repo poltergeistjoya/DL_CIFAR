f358a6ec2435a3ddf9f40b977a7d5375fde31ad5
Joya Debi <joya.debi@cooper.edu>
2022-09-28 16:30:06 +0000

    made all arrays

--- CIFARpreproc.py
+++ CIFARpreproc.py
@@ -6,11 +6,49 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-npdata = np.empty((0,3072))
+#unpickle all training
+npdata10 = np.empty((0,3072))
+nplabels10 = np.empty((0))
 for  i in range(0,5):
     batchdict = unpickle("./cifar-10-batches-py/data_batch_" + str(i+1))
-    npdata = np.append(npdata, batchdict[b'data'], axis=0)
-    #nplabels = batchdict[b'labels']
-    #print(type(nplabels), len(nplabels))
+    npdata10 = np.append(npdata10, batchdict[b'data'], axis=0)
+    trainlabels = np.array(batchdict[b'labels'])
+    nplabels10= np.append(nplabels10, trainlabels, axis = 0)
 
-print(npdata.shape)
+#npdata10 is now 50000 x 3072
+#nplabels10 is 50000 (1 dim)
+
+#unpickle test
+testdict10 = unpickle("./cifar-10-batches-py/test_batch")
+# 10000 x 3072
+nptestdata10 = testdict10[b'data']
+# 10000 (1 dim)
+nptestlabels10 = np.array(testdict10[b'labels'])
+
+np.savetxt("traindata10.txt", npdata10)
+np.savetxt("trainlabel10.txt", nplabels10)
+np.savetxt("testdata10.txt", nptestdata10)
+np.savetxt("testlabel10.txt", nptestlabels10)
+
+#now CIFAR100
+traindict100 = unpickle("./cifar-100-python/train")
+print(traindict100.keys())
+nptraindata100 = traindict100[b'data']
+print(nptraindata100.shape)
+# 10000 (1 dim)
+nptrainlabels100 = np.array(traindict100[b'fine_labels'])
+print(nptrainlabels100.shape)
+
+testdict100 = unpickle("./cifar-100-python/test")
+# 10000 x 3072
+nptestdata100 = testdict100[b'data']
+# 10000 (1 dim)
+nptestlabels100 = np.array(testdict100[b'fine_labels'])
+
+np.savetxt("traindata100.txt", nptraindata100)
+np.savetxt("trainlabel100.txt", nptrainlabels100)
+np.savetxt("testdata100.txt", nptestdata100)
+np.savetxt("testlabel100.txt", nptestlabels100)
+
+
+
